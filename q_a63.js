6e69623a0199de513e75168ea5841ac2bccb4569
hbjORbj <sldisek783@gmail.com>
2020-10-24 17:42:02 +0100

    Q63 Update

--- q_a63.js
+++ q_a63.js
@@ -7,14 +7,13 @@ top() -- Get the top element.
 getMin() -- Retrieve the minimum element in the stack.
 
 Qs to ask:
-1. 
+1. What to do if pop, top and getMin operations are called on empty stacks?
+- They will be called on non-empty stacks.
 */
 
-/**
- * initialize your data structure here.
- */
 var MinStack = function() {
-    
+    this.stack = [];
+    this.min = [];
 };
 
 /** 
@@ -22,26 +21,34 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(x) {
-    
+    if (this.min.length == 0) this.min.push(x);
+    else this.min.push(Math.min(x, this.getMin()));
+    this.stack.push(x);
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-    
+    this.stack.pop();
+    this.min.pop();
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    
+    return this.stack[this.stack.length-1];
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    
+    return this.min[this.min.length-1];
 };
+
+/*
+Time Complexity: O(1)
+Space Complexity: O(n)
+*/
