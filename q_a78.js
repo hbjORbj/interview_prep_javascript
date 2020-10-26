832e89ba10291716fcb4adf49f27d1f7268af889
hbjORbj <sldisek783@gmail.com>
2020-10-26 07:52:01 +0000

    Q78 Update

--- q_a78.js
+++ q_a78.js
@@ -1,12 +1,30 @@
 /*
+Reverse a linked list from position m to n. Do it in one-pass.
 
+Note: 1 ≤ m ≤ n ≤ length of list.
 */
 
-/*
-Test Cases:
+var reverseBetween = function(head, m, n) {
+    let start = head, cur = head;
+    let curPos = 1;
+    while (curPos < m) {
+        start = cur;
+        cur = cur.next;
+        curPos++;
+    }
 
-Idea:
-
-Time Complexity:
-Space Complexity:
-*/
+    let prev = null, tail = cur;
+    while (curPos <= n) {
+        let next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+        curPos++;
+    }
+    start.next = prev;
+    tail.next = cur;
+    
+    return m == 1 ? prev : head;
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+};
