c88ee0f6f41a696dd2133ac279072479f51d900f
hbjORbj <sldisek783@gmail.com>
2020-10-26 04:01:40 +0000

    Q76 Update

--- q_a76.js
+++ q_a76.js
@@ -12,13 +12,43 @@ After you are done modifying the input array, return the new length of the array
  
 Follow up:
 Could you solve it using only O(1) extra space?
-*/
 
+Qs:
+1. What to return if the input array is empty?
+- Leave it unchanged and return 0.
+2. If there are no consecutive elements, the output must be the same as the input, right?
+- Yes.
+*/
+var compress = function(chars) {
+    let start = 0, end = 0;
+    while (end < chars.length) {
+        if (chars[end] == chars[end+1]) end++;
+        else {
+            let len = end-start+1; // number of letters
+            if (len == 1) {
+                start++, end++;
+                continue;
+            }
+            
+            chars.splice(start+1, len-1, ...len.toString().split(""));
+            end -= len - 1 - len.toString().length; 
+            // move backwards as much as the number of letters 
+            // except for one letter char and length chars
+            
+            start = end; // bring start pointer to end pointer
+            start++, end++; // move both pointers to new letter
+        }
+    }
+    return chars.length;
+};
 /*
 Test Cases:
+['a','b','b'] => 3 ['a','b','2']
+['a','a','b','b','b','b','b','b','b','b','b','b'] => 5 ['a','2','b','1','0']
+[] => 0 []
 
-Idea:
-
-Time Complexity:
-Space Complexity:
+One Pass
+Two Pointer
+Time Complexity: O(n)
+Space Complexity: O(1)
 */
