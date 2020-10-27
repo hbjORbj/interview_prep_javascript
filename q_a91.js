c777dca7f509afff74d41ebb43fc05ce256f90fc
hbjORbj <sldisek783@gmail.com>
2020-10-27 18:27:00 +0000

    Q92 Update

--- q_a91.js
+++ q_a91.js
@@ -11,17 +11,19 @@ function insertFive(N) {
     return Number(newNum);
 }
 
-function getInsertIdx(num, N) {
-    let insertIdx = -1;
-    for (let i = 0; i < num.length; i++) {
+function getInsertIdx(numAsStr, N) {
+    let insertIdx = numAsStr.length; 
+    // in case that there is no smaller number in positive number
+    // in case that there is no larger number in negative number
+    for (let i = 0; i < numAsStr.length; i++) {
         if (N < 0) {
-            if (5 < Number(num[i])) {
+            if (5 < Number(numAsStr[i])) {
                 insertIdx = i;
                 break;
             }
         }
         else {
-            if (5 > Number(num[i])) {
+            if (5 > Number(numAsStr[i])) {
                 insertIdx = i;
                 break;
             }
@@ -34,6 +36,9 @@ console.log(insertFive(268));
 console.log(insertFive(680));
 console.log(insertFive(0));
 console.log(insertFive(-999));
+console.log(insertFive(-444));
+console.log(insertFive(666));
+
 
 /*
 Test Cases:
@@ -41,10 +46,14 @@ Test Cases:
 680 => 6850
 0 => 50
 -999 => -5999
+-444 => -4445
+666 => 6665
 
 Idea:
 1. For positive numbers, put 5 before the first smaller number
+- if there is no smaller number, we put 5 in the end
 2. For negative numbers, put 5 before the first larger number
+- if there is no greater number, we put 5 in the end
 
 Time Complexity: O(1)
 Space Complexity: O(1)
--- q_a92.js
+++ q_a92.js
@@ -1,9 +1,37 @@
 /*
 Given two integers A and B, 
 return the number of integers from the range [A,,,B] inclusive that can be expressed as the product of two consecutive integers.
-*/
 
+Qs:
+1. Can A and B be negative?
+- No, there will be positive.
+2. If B is less than A, what to return?
+- 0.
+*/
+function countProducts(A, B) {
+    let start = Math.floor(Math.sqrt(A));
+    let end = Math.floor(Math.sqrt(B));
+    let count = 0;
+    while (start <= end) {
+        let product = start*(start+1);
+        if (product >= A && product <= B) {
+            count++;
+        }
+        start++;
+    }
+    return count;
+}
+console.log(countProducts(6, 20))
+console.log(countProducts(1, 20))
+console.log(countProducts(1, 1))
+console.log(countProducts(0, 0))
 /*
 Test Cases:
-(6, 20) => 3
+(6, 20) => 3 [6,12,20]
+(1, 20) => 4 [2, 6,12,20]
+(1, 1) => 0 []
+(0, 0) => 1 [0]
+
+Consecutive numbers less than Math.sqrt(A) cannot produce products that are greater than or equal to A
+Consecutive numbers greater than Math.sqrt(B) cannot produce products that are less than or equal to B
 */
