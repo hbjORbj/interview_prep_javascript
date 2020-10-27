0b672a4c7ddc983d6fcdc7f19464f7e163abbe03
hbjORbj <sldisek783@gmail.com>
2020-10-27 00:26:28 +0000

    Q85 Update

--- q_a85.js
+++ q_a85.js
@@ -12,9 +12,17 @@ He will inform his direct subordinates and they will inform their subordinates a
 The i-th employee needs informTime[i] minutes to inform all of his direct subordinates (i.e After informTime[i] minutes, all his direct subordinates can start spreading the news).
 
 Return the number of minutes needed to inform all the employees about the urgent news.
+
+Qs:
+1. Every employee has only one manager, right?
+- Yes.
+2. Every employee except the head always has a manager, right?
+- Yes.
 */
+// n-ary Tree structure
 
 var numOfMinutes = function(n, headID, manager, informTime) {
+    // converting given data into graph in adjacency list
     let hierarchy = new Array(n);
     for (let i = 0; i < n; i++) hierarchy[i] = []; // initialise with empty array
 
@@ -41,5 +49,37 @@ var numOfMinutes = function(n, headID, manager, informTime) {
 };
 
 /*
+Test Cases:
 ( n = 6, headID = 2, manager = [2,2,-1,2,2,2], informTime = [0,0,1,0,0,0] ) => 1
+( n = 1, headID = 0, manager = [-1], informTime = [0] ) => 0 // edge case (there is only head and no one else)
 */
+
+var numOfMinutes = function(n, headID, manager, informTime) {
+    // converting given data into graph in adjacency list
+    let hierarchy = new Array(n);
+    for (let i = 0; i < n; i++) hierarchy[i] = []; // initialise with empty array
+
+    // index represents manager's id and values represent the manager's subordinates' id's
+    for (let i = 0; i < manager.length; i++) {
+        let managerId = manager[i];
+        if (managerId !== -1) hierarchy[managerId].push(i);
+        // except for head who has no manager
+    }
+    
+    let max = 0;
+    function dfs(curID, count) {
+        let subordinates = hierarchy[curID];
+        if (subordinates.length == 0) {
+            max = Math.max(max, count);
+            return;
+        }
+        for (let subordinate of subordinates) {
+            dfs(subordinate, count+informTime[curID]);
+        }
+    }
+    dfs(headID,0);
+    return max;
+    // DFS
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+}
