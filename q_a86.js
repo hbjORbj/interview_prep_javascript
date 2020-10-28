4f23be1c3a891e981fa3b7ca62e31be7f86d0ab7
hbjORbj <sldisek783@gmail.com>
2020-10-28 03:27:26 +0000

    Q86 Update

--- q_a86.js
+++ q_a86.js
@@ -104,3 +104,38 @@ var canFinish = function(numCourses, prerequisites) {
     // Time Complexity: O(n)
     // Space Complexity: O(n)
 };
+
+var canFinish = function(numCourses, prerequisites) {
+    let adjList = new Array(numCourses).fill(0).map(() => []);
+    let inDegree = new Array(numCourses).fill(0);
+    let totalDegree = 0;
+    for (let pair of prerequisites) {
+        let course = pair[0];
+        let prerequisite = pair[1];
+        adjList[prerequisite].push(course);
+        inDegree[course]++;
+        totalDegree++;
+    }
+    let stack = [], count = 0;
+    for (let i = 0; i < inDegree.length; i++) {
+        if (inDegree[i] == 0) stack.push(i);
+    }
+    
+    while (stack.length > 0) {
+        let v = stack.pop();
+        count++;
+        let courses = adjList[v];
+        for (let course of courses) {
+            inDegree[course]--;
+            totalDegree--;
+            if (inDegree[course] == 0) {
+                stack.push(course);
+            }
+        }
+    }
+
+    return count == numCourses;
+    // Topological sort
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+};
