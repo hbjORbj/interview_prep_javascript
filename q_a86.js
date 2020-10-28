586b5d77fe9652586034edff32c2a4129e5d34e3
hbjORbj <sldisek783@gmail.com>
2020-10-28 03:19:33 +0000

    Q86 Update

--- q_a86.js
+++ q_a86.js
@@ -42,3 +42,65 @@ Test Cases:
 2, [[1,0], [0,1]] => false
 3, [[1,0], [2, 1]] => true
 */
+
+var canFinish = function(numCourses, prerequisites) {
+    let graph = new Array(numCourses).fill(0).map(() => []);
+    for (let pair of prerequisites) {
+        let course = pair[0];
+        let prerequisite = pair[1];
+        graph[prerequisite].push(course);
+    }
+    
+    for (let v = 0; v < numCourses; v++) {
+        let visited = [], queue = [];
+        queue.push(graph[v]);
+        
+        while (queue.length > 0) {
+            let courses = queue.shift();
+            for (let course of courses) {
+                if (!visited[course]) {
+                    if (v == course) return false;
+                    visited[course] = true;
+                    queue.push(graph[course]);
+                }
+            }       
+        }
+    }
+    return true;
+    // BFS
+    // Time Complexity: O(n^2)
+    // Space Complxity: O(n)
+};
+
+var canFinish = function(numCourses, prerequisites) {
+    let adjList = new Array(numCourses).fill(0).map(() => []);
+    let inDegree = new Array(numCourses).fill(0);
+    let totalDegree = 0;
+    for (let pair of prerequisites) {
+        let course = pair[0];
+        let prerequisite = pair[1];
+        adjList[prerequisite].push(course);
+        inDegree[course]++;
+        totalDegree++;
+    }
+    let stack = [];
+    for (let i = 0; i < inDegree.length; i++) {
+        if (inDegree[i] == 0) stack.push(i);
+    }
+    
+    for (let v of stack) {
+        let courses = adjList[v];
+        for (let course of courses) {
+            inDegree[course]--;
+            totalDegree--;
+            if (inDegree[course] == 0) {
+                stack.push(course);
+            }
+        }
+    }
+
+    return totalDegree == 0;
+    // Topological sort
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+};
