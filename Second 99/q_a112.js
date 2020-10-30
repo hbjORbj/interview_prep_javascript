6bf722e19e7006727c3ea7ad23dfc8ba838be861
hbjORbj <sldisek783@gmail.com>
2020-10-30 23:12:08 +0000

    Q112 Update

---
+++ Second 99/q_a112.js
@@ -0,0 +1,15 @@
+/*
+Given an array A of N integers, return the smallest positive integer (greater than 0) that does not occur in A.
+
+Ex) A = [1,3,6,4,1,2] should return 5.
+
+Qs:
+1. Can there be duplicates?
+- Yes.
+2. Can there be negative integers?
+- Yes.
+*/
+
+function smallestPositive(A) {
+
+}
