d056a9953ce56c27e8ae427d50e8a07045111fbb
hbjORbj <sldisek783@gmail.com>
2020-10-30 23:07:52 +0000

    Q111 Update

--- Second 99/q_a111.js
+++ Second 99/q_a111.js
@@ -7,29 +7,33 @@ ex) (ids: [2,3,1,1,1,3,4], m: 2) => 2 (you can take out 2 and 4)
 */
 
 function minUniqueIds(ids, m) {
+    if (ids.length == 0) return 0;
     let map = new Map();
     for (let id of ids) {
         map.set(id, map.get(id)+1 || 1);
     }
     let numOfUniqueIds = map.size;
     let frequencies = Array.from(map.values()).sort((a,b) => a-b);
-    let j = 0;
-    for (let i = 0; i < m; i++) {
-        frequencies[j]--;
-        if (frequencies[j] == 0) {
-            numOfUniqueIds--;
-            j++;
+    let i = 0;
+    while (m > 0) { // runs n (length of ids) times in the worst case
+        if (frequencies[i] > m) {
+            break;
         }
+        m -= frequencies[i++];
+        numOfUniqueIds--;
     }
     return numOfUniqueIds;
 }
-
+// Time Complexity: O(nlog(n))
+// Space Complexity: O(n)
 console.log(minUniqueIds([2,3,1,1,1,3,4], 2));
 console.log(minUniqueIds([], 2));
 console.log(minUniqueIds([2,3], 2));
 console.log(minUniqueIds([1,1,1,1,2,2,2,2,2], 3 ));
+console.log(minUniqueIds([1,1,1,1,2,2,2,2,2], 7 ));
 
 // [2,3,1,1,1,3,4], 2 => 2
 // [], 2 => 0
 // [2,3], 2 => 0
 // [1,1,1,1,2,2,2,2,2], 3  => 2
+// [1,1,1,1,2,2,2,2,2], 7 => 1
