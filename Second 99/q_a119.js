2829a6823de3d0bcd13632bc170f56bdfe5c40ed
hbjORbj <sldisek783@gmail.com>
2020-10-31 10:25:35 +0000

    Q119 Update

--- Second 99/q_a119.js
+++ Second 99/q_a119.js
@@ -11,6 +11,43 @@ Examples:
 - Given A = [51, 32, 43], the function should return -1.
 
 Qs:
-1. Can there be negative integers?
+1. Can there be negative integers or zeros?
 - No. There are only positive integers.
 */
+
+console.log(maxSumWithEqualDigitSum([51, 71, 17, 42])); // => 93
+console.log(maxSumWithEqualDigitSum([42, 33, 60])); // => 102
+console.log(maxSumWithEqualDigitSum([51, 32, 43])); // => -1
+console.log(maxSumWithEqualDigitSum([1, 2, 3])); // => -1
+console.log(maxSumWithEqualDigitSum([51, 4000, 1030])); // => 5030
+console.log(maxSumWithEqualDigitSum([51293, 2332, 433])); // => 2765
+console.log(maxSumWithEqualDigitSum([])); // => -1
+
+function maxSumWithEqualDigitSum(A) {
+    let m = new Map();
+    let maxSum = -1;
+    for (let num of A) {
+        let digitSum = addDigits(num);
+        if (m.has(digitSum)) {
+            let prevMax = m.get(digitSum);
+            maxSum = Math.max(maxSum, prevMax + num);
+            m.set(digitSum, Math.max(prevMax, num));
+        }
+        else {
+            m.set(digitSum, num);
+        }
+    }
+    return maxSum;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+function addDigits(num) {
+    let sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num = Math.floor(num / 10);
+    }
+    return sum;
+}
