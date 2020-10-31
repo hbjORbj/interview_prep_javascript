278c9bb7d3fbdd97054c8f12ae70efb0d43e6fac
hbjORbj <sldisek783@gmail.com>
2020-10-31 04:41:12 +0000

    Bubble sort Update

--- Second 99/q_a114.js
+++ Second 99/q_a114.js
@@ -14,9 +14,9 @@ function bubbleSort1(A) { // sorts the array in ascending order
     }
     return A;
 }
-console.log(bubbleSort([4,1,9,2]));
-console.log(bubbleSort([2,-1,3,5]));
-console.log(bubbleSort([]));
+// console.log(bubbleSort1([4,1,9,2]));
+// console.log(bubbleSort1([2,-1,3,5]));
+// console.log(bubbleSort1([]));
 
 /*
 Test Cases:
@@ -29,5 +29,19 @@ Space Complexity: O(1)
 */  
 
 function bubbleSort2(A) { // sorts the array in descending order
-
+    while (true) { // runs n times in the worst case: ex) [1,2,3,4]
+        let swap = 0;
+        for (let i = 0; i < A.length-1; i++) {
+            if (A[i] < A[i+1]) {
+                [A[i], A[i+1]] = [A[i+1], A[i]];
+                swap++;
+            }
+        }
+        if (swap === 0) break;
+    }
+    return A;
 }
+console.log(bubbleSort2([4,1,9,2]));
+console.log(bubbleSort2([2,-1,3,5]));
+console.log(bubbleSort2([]));
+
