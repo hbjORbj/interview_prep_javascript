2d4f74524ea9c19270f7d99e23652b171c804640
hbjORbj <sldisek783@gmail.com>
2020-10-31 04:46:05 +0000

    Selection sort Update

--- Second 99/q_a115.js
+++ Second 99/q_a115.js
@@ -2,7 +2,7 @@
 Implement a selection sort.
 */
 
-function selectionSort(A) {
+function selectionSort1(A) { // sorts the array in ascending order
     let swapIdx = 0; // represents the index where the next smallest number will go into
     while (swapIdx < A.length) { // runs n times "always"
         let min = Infinity, minIdx = -1;
@@ -18,10 +18,10 @@ function selectionSort(A) {
     return A;
 }
 
-console.log(selectionSort([4,1,9,2]));
-console.log(selectionSort([2,-1,3,5]));
-console.log(selectionSort([2,-1,9,-1,-2,-3,5]));
-console.log(selectionSort([]));
+// console.log(selectionSort1([4,1,9,2]));
+// console.log(selectionSort1([2,-1,3,5]));
+// console.log(selectionSort1([2,-1,9,-1,-2,-3,5]));
+// console.log(selectionSort1([]));
 /*
 Test Cases:
 [4,1,9,2] => [1,2,4,9]
@@ -34,3 +34,25 @@ We partition the array into two parts: sorted partition and unsorted partition.
 Time Complexity: O(n^2)
 Space Complexity: O(1)
 */  
+
+
+function selectionSort2(A) { // sorts the array in ascending order
+    let swapIdx = 0;
+    while (swapIdx < A.length) {
+        let max = -Infinity, maxIdx = -1;
+        for (let i = swapIdx; i < A.length; i++) {
+            if (A[i] > max) {
+                max = A[i];
+                maxIdx = i;
+            }
+        }
+        [A[maxIdx], A[swapIdx]] = [A[swapIdx], A[maxIdx]];
+        swapIdx++;
+    }
+    return A;
+}
+
+console.log(selectionSort2([4,1,9,2]));
+console.log(selectionSort2([2,-1,3,5]));
+console.log(selectionSort2([2,-1,9,-1,-2,-3,5]));
+console.log(selectionSort2([]));
