aae0e9619e4a2bd38df07e5c8dc879a34defe6fd
hbjORbj <sldisek783@gmail.com>
2020-11-02 11:37:23 +0000

    Q128 Improved Space Complexity

--- Second 99/q_a128.js
+++ Second 99/q_a128.js
@@ -9,22 +9,23 @@ Qs:
 */
 
 function maxInserts(S) {
-    let numOfAs = 0, inserts = 2;
-    let substr = "";
+    let numOfAs = 0, numOfConsecutiveAs = 0, inserts = 2;
     for (let i = 0; i < S.length; i++) {
         if (S[i] == 'a') {
             numOfAs++;
-            substr += S[i];
+            numOfConsecutiveAs++;
         }
         else {
             inserts += 2;
-            substr = "";
+            numOfConsecutiveAs = 0;
         }
-        if (substr.length >= 3) return -1; // 3 or greater consecutive a's exist
+        if (numOfConsecutiveAs >= 3) return -1; // 3 or greater consecutive a's exist
     }
     
     return inserts - numOfAs;
 }
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
 console.log(maxInserts("aabab")); // => 3
 console.log(maxInserts("dog")); // => 8
@@ -35,8 +36,8 @@ console.log(maxInserts("")); // => 2
 console.log(maxInserts("bababaabbbabab")); // => 12
 
 /*
-1. Keep track of possible substrings of consecutive a's in order to figure out if there is already
-a substring of a's of length greater than or equal to 3. If so, return -1. Else, continue scanning through the array.
+1. Keep track of number of consecutive a's in order to figure out if there is already a substring of a's of length 
+greater than or equal to 3. If so, return -1. Else, continue scanning through the array.
 2. The total number of inserts is the number of possible inserts assuming there is no existing a's - the number of a's. 
 The number of possible inserts increases by two for every non-a's. 
 Our variable "inserts" is initialised to 2, because this is the number of possible inserts assuming no existing a's,
