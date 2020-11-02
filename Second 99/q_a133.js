2e32a8908b6322e814daa156b66183a2261ca211
hbjORbj <sldisek783@gmail.com>
2020-11-02 15:08:17 +0000

    Qs Update

---
+++ Second 99/q_a133.js
@@ -0,0 +1,11 @@
+/*
+Given string str, 
+
+find the length of the longest substring of str such that no three consecutive characters in the substring are same.
+
+Ex) 
+str = “baaabbabbb” => 7
+str = “babba” => 5
+*/
+
+function maxLenSubStr
---
+++ Second 99/q_a134.js
@@ -0,0 +1,9 @@
+/*
+Given a string s containing only a and b, 
+
+find longest substring of s such that s does not contain more than two contiguous occurrences of a and b.
+
+Ex) 
+"aabbaaaaabb" => "aabbaa"
+"aabbaabbaabbaa" => "aabbaabbaabbaa"
+*/
