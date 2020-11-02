ed5163a47cef7a3eb57b2e8a1048e08b9a885567
hbjORbj <sldisek783@gmail.com>
2020-11-02 16:05:06 +0000

    Q134 Update

--- Second 99/q_a134.js
+++ Second 99/q_a134.js
@@ -7,3 +7,28 @@ Ex)
 "aabbaaaaabb" => "aabbaa"
 "aabbaabbaabbaa" => "aabbaabbaabbaa"
 */
+
+function maxLenSubStr(s) {
+    let start = 0, end = 0;
+    let numOfConsecutiveChars = 0;
+    let res = "";
+    while (end <= s.length) {
+        numOfConsecutiveChars++;
+        if (numOfConsecutiveChars > 2 || end == s.length) { // time to end our substring is these two cases
+            if (end-start > res.length) res = s.substring(start,end); // if it is not longer, there is no need to store it
+            start = end-1; // slide our window so we can look for a new valid substring
+            numOfConsecutiveChars--; // now we have a valid substring with two contiguous occurrences instead of three
+        }
+        if (s[end] !== s[end+1]) numOfConsecutiveChars = 0;
+        end++;
+    }
+    return res;
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+}
+
+console.log(maxLenSubStr("aabbaaaaabb")); // => "aabbaa"
+console.log(maxLenSubStr("aabbaabbaabbaa")); // => "aabbaabbaabbaa"
+console.log(maxLenSubStr("")); // => ""
+console.log(maxLenSubStr("bbbaaa")); // => "bbaa"
+console.log(maxLenSubStr("aabbaaaababababbb")); // => "aababababb"
