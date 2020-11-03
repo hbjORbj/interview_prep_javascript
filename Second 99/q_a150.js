bd7e7b5af784fc4723740e662fdabdb50a464950
hbjORbj <sldisek783@gmail.com>
2020-11-03 20:12:11 +0000

    Q150 Update

--- Second 99/q_a150.js
+++ Second 99/q_a150.js
@@ -6,3 +6,26 @@ Ex)
 =>
 1 -> 4 -> 3 -> 2 -> 5 -> NULL
 */
+
+var reverseBetween = function(head, m, n) {
+    let start = head, cur = head;
+    let i = 1;
+    while (i < m) {
+        start = cur;
+        cur = cur.next;
+        i++;
+    }
+    let prev = null, tail = cur;
+    while (i <= n) {
+        let next = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = next;
+        i++;
+    }
+    start.next = prev;
+    tail.next = cur;
+    return m == 1 ? prev : head;
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+};
