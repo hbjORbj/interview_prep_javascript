87f5e2d7753e4add8f83faf1f323068ef7bc4177
hbjORbj <sldisek783@gmail.com>
2020-11-04 15:20:49 +0000

    Q164 Update

---
+++ Second 99/q_a164.js
@@ -0,0 +1,20 @@
+/*
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+*/
+
+function levelOrder(root) {
+    if (root == null) return [];
+    let queue = [], values = [];
+    queue.push(root);
+    while (queue.length > 0) {
+        let len = queue.length, level = [];
+        for (let i = 0; i < len; i++) {
+            let node = queue.shift();
+            level.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        values.push(level);
+    }
+    return values;
+}
