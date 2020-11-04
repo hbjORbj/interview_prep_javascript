9b950a5b8021aae7397a61d7730fb44a64fc173a
hbjORbj <sldisek783@gmail.com>
2020-11-04 10:32:11 +0000

    Q156 Update

--- Second 99/q_a156.js
+++ Second 99/q_a156.js
@@ -49,3 +49,33 @@ console.log(numberOfIslands([
     ["1","1","0","0","0"],
     ["0","0","0","0","0"]
   ]));
+
+  var numIslands = function(grid) {
+    let height = grid.length, width = grid[0].length;
+    let count = 0;
+    const DIRECTIONS = [[-1,0],[0,1],[1,0],[0,-1]];
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            if (grid[row][col] == "1") {
+                count++;
+                traverse([[row,col]]);
+            }
+        }
+    }
+    return count;
+    
+    // BFS Traversal
+    function traverse(queue) {
+        while (queue.length > 0) {
+            let [row, col] = queue.shift();
+            if (row < 0 || row >= height || col < 0 || col >= width 
+                || grid[row][col] == '0') continue;
+            grid[row][col] = "0"; // mark so that we know we already visited it
+            for (let dir of DIRECTIONS) {
+                queue.push([row+dir[0], col+dir[1]]);
+            }
+        }
+    }
+    // Time Complexity: O(m*n)
+    // Space Complexity: O(m*n), queue might contain m*n elements if the entire grid is one island
+};
---
+++ Second 99/q_a160.js
@@ -0,0 +1,4 @@
+/*
+One string is strictly smaller than another when the frequency of occurrence of the smallest character in the string
+is less than the frequency of occurrence of the smallest character in the comparison string.
+*/
