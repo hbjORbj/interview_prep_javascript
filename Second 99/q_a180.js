435d9cf97547a3418d39308b99dafe5ec73e4d1a
hbjORbj <sldisek783@gmail.com>
2020-11-05 19:04:58 +0000

    Q180 Update

--- Second 99/q_a180.js
+++ Second 99/q_a180.js
@@ -5,3 +5,10 @@ instead you will be given access to the node to be deleted directly.
 
 It is guaranteed that the node to be deleted is not a tail node in the list.
 */
+
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+    // Time Complexity: O(1)
+    // Space Complexity: O(1)
+};
