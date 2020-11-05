ee49be33031e207e1c0956243f7e72940c4688cf
hbjORbj <sldisek783@gmail.com>
2020-11-05 18:44:35 +0000

    Q181 Update

---
+++ Second 99/q_a180.js
@@ -0,0 +1,7 @@
+/*
+Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, 
+
+instead you will be given access to the node to be deleted directly.
+
+It is guaranteed that the node to be deleted is not a tail node in the list.
+*/
