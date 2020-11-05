2254fb4ff55a73b82af1c534372b4877c225102f
hbjORbj <sldisek783@gmail.com>
2020-11-05 15:37:14 +0000

    Q174 Update

--- Second 99/q_a174.js
+++ Second 99/q_a174.js
@@ -4,3 +4,23 @@ Merge two sorted linked lists and return it as a new sorted list.
 The new list should be made by splicing together the nodes of the first two lists.
 */
 
+var mergeTwoLists = function(l1, l2) {
+    let dummy = new ListNode();
+    let cur = dummy;
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            cur.next = l1;
+            l1 = l1.next;
+        } else {
+            cur.next = l2;
+            l2 = l2.next;
+        }
+        cur = cur.next;
+    }
+    if (l1) cur.next = l1;
+    else if (l2) cur.next = l2;
+    
+    return dummy.next;
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+};
