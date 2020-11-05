5733119835a903d66ff4bb6a61d0d876358958c7
hbjORbj <sldisek783@gmail.com>
2020-11-05 13:21:03 +0000

    Q176 Update

--- Second 99/q_a176.js
+++ Second 99/q_a176.js
@@ -17,4 +17,8 @@ var removeElements = function(head, val) {
     return dummy.next;
     // Time Complexity: O(n)
     // Space Complexity: O(1)
+    // Scan through the linked list and append all nodes whose values are different
+    // from "val" to current list which we will return at the end
+    // Make sure the last node point to null since it might point to a node
+    // whose value is equal to "val"
 };
