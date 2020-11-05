6a4a2abdf9bcb44005cf7c9c2e1fcc786ff524ee
hbjORbj <sldisek783@gmail.com>
2020-11-05 17:27:08 +0000

    Q172 Update

--- Second 99/q_a172.js
+++ Second 99/q_a172.js
@@ -7,3 +7,48 @@ following the next pointer.
 
 Notice that you should not modify the linked list.
 */
+
+var detectCycle = function(head) {
+    let len = lengthOfCycle(head);
+    if (len == 0) return null; // there is no cycle
+
+    let ptr1 = head, ptr2 = head;
+
+    for (let i = 0; i < len; i++) { // advance ptr2 ahead by the length of cycle
+        ptr2 = ptr2.next;
+    }
+
+    while (ptr1 !== ptr2) { // both pointers start to advance by one and 
+                            // where they meet is the beginning of cycle
+        ptr1 = ptr1.next;
+        ptr2 = ptr2.next;
+    }
+    return ptr1;
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+};
+
+function lengthOfCycle(head) {
+    let slow = head, fast = head;
+    
+    // check if there is cycle
+    // if so, slow == fast when the loop breaks,
+    // else, fast == null
+    while (fast !== null) {
+        fast = fast.next;
+        if (slow == fast) break;
+        if (fast !== null) fast = fast.next;
+        if (slow == fast) break;
+        slow = slow.next;
+    }
+
+    let len = 0;
+    
+    // find the length of cycle
+    while (fast !== null) {
+        fast = fast.next; 
+        len++;
+        if (slow == fast) break;
+    }
+    return len;
+};
