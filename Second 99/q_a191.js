94e0776d4abc99b838cfc1cc9bd71f2cedd362ec
hbjORbj <sldisek783@gmail.com>
2020-11-06 17:57:31 +0000

    Q191 Update

--- Second 99/q_a191.js
+++ Second 99/q_a191.js
@@ -2,6 +2,7 @@
 Write a program to find the node at which the intersection of two singly linked lists begins.
 */
 
+/*
 var getIntersectionNode = function(headA, headB) {
     let m = new Map();
     while (headA !== null) {
@@ -16,3 +17,35 @@ var getIntersectionNode = function(headA, headB) {
     // Time Complexity: O(m+n), m = length of list A, n = length of list B
     // Space Complexity: O(max(m,n))
 };
+*/
+
+var getIntersectionNode = function(headA, headB) {
+    let len1 = getLength(headA), len2 = getLength(headB);
+    if (len1 < len2) {
+        for (let i = 0; i < len2-len1; i++) {
+            headB = headB.next;
+        }    
+    }
+    else {
+        for (let i = 0; i < len1-len2; i++) {
+            headA = headA.next;
+        }
+    }
+    while (headA !== null) {
+        if (headA == headB) return headA;
+        headA = headA.next;
+        headB = headB.next;
+    }
+    return null;
+    // Time Complexity: O(m+n)
+    // Space Complexity: O(1)
+}
+
+function getLength(head) {
+    let len = 0;
+    while (head !== null) {
+        head = head.next;
+        len++;
+    }
+    return len;
+}
