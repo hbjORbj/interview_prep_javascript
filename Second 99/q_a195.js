99d18fe1b4170c5893d631e949804c9fc175f0f1
hbjORbj <sldisek783@gmail.com>
2020-11-07 11:29:12 +0000

    Q195 Update

---
+++ Second 99/q_a195.js
@@ -0,0 +1,36 @@
+/*
+Given a binary tree, return all values given a certain height H.
+*/
+
+// BFS
+function valuesAtDepthH(root, H) {
+    let queue = [], values = [];
+    queue.push({node: root, depth: 1});
+
+    while (queue.length > 0) {
+        let {node, depth} = queue.shift()
+        if (depth == H) {
+            values.push(node.val);
+            if (queue.length == 0) break; // optimisation
+        }
+        if (node.left) queue.push({node: node.left, depth: depth + 1});
+        if (node.right) queue.push({node: node.right, depth: depth + 1});
+    }
+    return values;
+}
+
+//DFS
+function valuesAtDepthH(root, H) {
+    let values = [];
+    function dfsTraversal(root, depth) {
+        if (!root) return;
+        if (depth == H) {
+            values.push(root.val);
+            return;
+        }
+        dfsTraversal(root.left, depth+1);
+        dfsTraversal(root.right, depth+1);
+    }
+    dfdsTraversal(root, 1);
+    return values;
+}
