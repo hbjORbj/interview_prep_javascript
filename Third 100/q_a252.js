ded35979eecbdd55e8b179bf408f56c701862e8f
hbjORbj <sldisek783@gmail.com>
2020-11-14 05:38:54 +0000

    Q252 Update

--- Third 100/q_a252.js
+++ Third 100/q_a252.js
@@ -3,3 +3,33 @@ Given a binary tree root and an integer target, delete all the leaf nodes with v
 
 Note that once you delete a leaf node with value target, if it's parent node becomes a leaf node and has the value target, it should also be deleted (you need to continue doing that until you can't).
 */
+
+var removeLeafNodes = function(root, target) {
+    dfs(root, target);
+    if (root.val == target && isLeaf(root)) return null;
+    else return root;
+    // Time Complexity: O(N), we visit all nodes
+    // Space Complexity: O(H), call stack can go as deep as height of tree
+};
+
+function dfs(root, target) {
+    if (!root) return;
+    if (root.left && root.left.val == target && isLeaf(root.left)) {
+        root.left = null;
+    }
+    if (root.right && root.right.val == target && isLeaf(root.right)) {
+        root.right = null;
+    }
+    dfs(root.left, target);
+    if (root.left && root.left.val == target && isLeaf(root.left)) {
+        root.left = null;
+    }
+    dfs(root.right, target);
+    if (root.right && root.right.val == target && isLeaf(root.right)) {
+        root.right = null;
+    }
+}
+
+function isLeaf(root) {
+    return !root.left && !root.right;
+}
