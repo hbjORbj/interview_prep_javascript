8a14337c5e405d7130dc09c0d03f4d19a430e105
hbjORbj <sldisek783@gmail.com>
2020-11-16 20:16:36 +0000

    Q273 Update

--- Third 100/q_a273.js
+++ Third 100/q_a273.js
@@ -6,28 +6,26 @@ You need to merge them into a new binary tree. The merge rule is that if two nod
 Otherwise, the NOT null node will be used as the node of new tree.
 */
 
-// We will create a function that takes two trees and return the merged tree.
-// If both root nodes exist, we add up their values for the root of our new, merged tree.
-// Else, we take the value of either of two roots for the root of our new, merged tree.
-// We perform this function on left subtrees of both roots and the result becomes our root's left subtree. We do the same for our root's right subtree. We return the root now.
-
 var mergeTrees = function(t1, t2) {
-    if (!t1 || !t2) return t1 || t2;
     return merge(t1, t2);
     // Time Complexity: O(min(m, n)), m = # of nodes in t1 and n = # of nodes in t2
     // Space Complexity: O(m + n)
 };
 
 function merge(node1, node2) {
-    if (!node1 && !node2) return null;
-    let val = (node1 && node2) ? (node1.val + node2.val) : (node1.val || node2.val);
-    let root = new TreeNode(val);
-    root.left = (node1.left && node2.left) ? merge(node1.left, node2.left) : (node1.left || node2.left);
-    root.right =  (node1.right && node2.right) ? merge(node1.right, node2.right) : (node1.right || node2.right);
+    if (!node1 || !node2) return node1 || node2;
+    let root = new TreeNode(node1.val + node2.val);
+    root.left = merge(node1.left, node2.left);
+    root.right = merge(node1.right, node2.right);
     return root;
 }
 
 /***************************************************************************************************/
+
+// To merge two trees, we need to merge the root node and left subtree and right subtree.
+// Two cases exist when we try to merge the root nodes. If both nodes exist, we simply add t2's value into t1's value. Else, we have to use either of the two.
+// Now, how do we merge the left subtree and right subtree? 
+// We simply need to perform this same function on the left subtrees of the two trees and the right subtrees of the two trees. As a result, we have merged root node, merged left subtree and merged right subtree. Done!
 
 var mergeTrees = function(t1, t2) {
     return merge(t1, t2);
