a6b91bf03c0d7e9cfa9a314e3cefa7e510337571
hbjORbj <sldisek783@gmail.com>
2020-11-18 16:41:52 +0000

    Q281 Update

--- Third 100/q_a281.js
+++ Third 100/q_a281.js
@@ -11,3 +11,61 @@ If M[i][j] = 1, then the ith and jth students are direct friends with each other
 
 And you have to output the total number of friend circles among all the students.
 */
+
+// Idea: Every time we encounter 1, we need to find the connected component starting from this node. 
+// Why? 1 means that this person is a direct friend of mine. 
+// Now we want to visit all of his friends, all of his friends' friends and so on because all these people are my indirect friends and we together make one circle.
+
+var findCircleNum = function(M) {
+    if (M.length == 0 || M[0].length == 0) return 0;
+    let circles = 0;
+    let visited = new Array(M.length).fill(false); // to check whether we've already visited a vertex
+    for (let vertex = 0; vertex < M.length; vertex++) {
+        if (!visited[vertex]) {
+            bfs(vertex, M, visited);
+            circles++;
+        }
+    }
+    return circles;
+    // Time Complexity: O(N^2), we scan through every node once
+    // Space Complexity: O(N), for "visited" array
+};
+
+function bfs(vertex, grid, visited) {
+    let queue = [vertex];
+    while (queue.length > 0) {
+        let from = queue.shift();
+        visited[from] = true;
+        for (let to = 0; to < grid[from].length; to++) {
+            if (grid[from][to] == 1 && !visited[to]) {
+                queue.push(to);
+            }
+        }
+    }
+};
+/****************************************************************************************************************/
+
+var findCircleNum = function(M) {
+    if (M.length == 0 || M[0].length == 0) return 0;
+    let circles = 0;
+    let visited = new Array(M.length).fill(false);
+    for (let vertex = 0; vertex < M.length; vertex++) {
+        if (!visited[vertex]) {
+            dfs(vertex, M, visited);
+            circles++;
+        }
+    }
+    return circles;
+    // Time Complexity: O(N^2), we visit every node once
+    // Space Complexity: O(N), for "visited" array
+};
+
+function dfs(vertex, grid, visited) {
+    visited[vertex] = true;
+    
+    for (let to = 0; to < grid[vertex].length; to++) {
+        if (grid[vertex][to] == 1 && !visited[to]) {
+            dfs(to, grid, visited);
+        }
+    }
+}
--- Third 100/q_a285.js
+++ Third 100/q_a285.js
@@ -7,28 +7,3 @@ Given an array arr of positive integers, consider all binary trees such that:
 
 Among all possible binary trees considered, return the smallest possible sum of the values of each non-leaf node.  It is guaranteed this sum fits into a 32-bit integer.
 */
-
-var findCircleNum = function(M) {
-    let circles = 0;
-    let visited = new Array(M.length).fill(false);
-    for (let row = 0; row < M.length; row++) {
-        if (visited[row] == false) {
-            dfs(row, M, visited);
-            circles++;
-        }
-    }
-    return circles;
-    // Time Complexity: O(N^2), we visit every node once
-    // Space Complexity: O(N)
-};
-
-function dfs(vertex, M, visited) {
-    visited[vertex] = true;
-    let connections = M[vertex];
-    for (let next = 0; next < connections.length; next++) {
-        if (M[vertex][next] == 1 && visited[next] == false) { // there is an edge from current vertex to next vertex
-            dfs(next, M, visited);
-        }
-    }
-}
-// Idea: Every time we encounter 1, we need to find the connected component starting from this node.
