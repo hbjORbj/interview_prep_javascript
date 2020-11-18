2d2371fcb33ca39ee5553b88a7dd8eed3cd0e478
hbjORbj <sldisek783@gmail.com>
2020-11-18 00:45:21 +0000

    Q279 Update

--- Third 100/q_a279.js
+++ Third 100/q_a279.js
@@ -12,9 +12,8 @@ Create a timebased key-value store class TimeMap, that supports two operations.
 - If there are no values, it returns the empty string ("").
 */
 
-/**
- * Initialize your data structure here.
- */
+// This solution works even if the following assumption is not held: the timestamps for all TimeMap.set operations are strictly increasing.
+
 var TimeMap = function() {
     this.map = new Map();
 };
@@ -26,8 +25,7 @@ var TimeMap = function() {
  * @return {void}
  */
 
-// (key, value) in map represents (key, values[timestamp]) where values[timestamp] is the value given with the particular timestamp.
-// We are using timestamp as index.
+// (key, value) in map represents (key, values[timestamp]) where values[timestamp] is the value given with the particular timestamp. We are using timestamp as index.
 TimeMap.prototype.set = function(key, value, timestamp) {
     if (!this.map.has(key)) this.map.set(key, []);
     let values = this.map.get(key);
@@ -49,3 +47,51 @@ TimeMap.prototype.get = function(key, timestamp) {
     }
     return "";
 };
+// Time Complexity: O(1) for set() and O(K) for get() where K = maximum length of values[] array among entries in the TimeMap
+// Space Complexity: O(N), N = # of entries in the TimeMap
+
+/**************************************************************************************************************/
+
+// This solution works if and only if the following assumption is held: the timestamps for all TimeMap.set operations are strictly increasing.
+
+var TimeMap = function() {
+    this.map = new Map();
+};
+
+/** 
+ * @param {string} key 
+ * @param {string} value 
+ * @param {number} timestamp
+ * @return {void}
+ */
+
+TimeMap.prototype.set = function(key, value, timestamp) {
+    if (!this.map.has(key)) this.map.set(key, []);
+    this.map.get(key).push([timestamp, value]);
+};
+
+/** 
+ * @param {string} key 
+ * @param {number} timestamp
+ * @return {string}
+ */
+
+TimeMap.prototype.get = function(key, timestamp) {
+    if (!this.map.has(key)) return "";
+    let values = this.map.get(key);
+    let left = 0, right = values.length - 1, mid;
+    while (left < right) {
+        mid = left + Math.ceil((right - left) / 2);
+        if (values[mid][0] === timestamp) {
+            return values[mid][1];
+        }
+        else if (values[mid][0] < timestamp) {
+            left = mid; // could be the biggest valid value in the array so we include it for next iteration
+        } else {
+            right = mid - 1; // we can't accept a value greater than timestamp so no need to include it for next iteration
+        }
+    }
+    return (values[left][0] <= timestamp) ? values[left][1] : "";
+};
+// Time Complexity: O(1) for set() and O(log(K)) for get() where K = maximum length of values[] array among entries in the TimeMap
+// Space Complexity: O(N), N = # of entries in the TimeMap
