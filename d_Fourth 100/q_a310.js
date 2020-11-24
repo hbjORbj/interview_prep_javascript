6a2d2a564bba5425da7ed2ffa763a9f62587bd16
hbjORbj <sldisek783@gmail.com>
2020-11-24 19:12:19 +0000

    Q310 Update

--- d_Fourth 100/q_a310.js
+++ d_Fourth 100/q_a310.js
@@ -24,3 +24,32 @@ In other words, output[i] = prefixProduct[i-1] * suffixProduct[i+1].
 prefixProduct[i] is the cumulative product of numbers including itself starting from the beginning.
 suffixProduct[i] is the cumulative product of numbers including itself starting from the end.
 */
+
+var productExceptSelf = function(nums) {
+    if (nums.length <= 1) return nums;
+    let output = [];
+    
+    let prefixProduct = 1;
+    for (let i = 0; i < nums.length; i++) {
+        output[i] = prefixProduct;
+        prefixProduct *= nums[i];
+    }
+    
+    let suffixProduct = 1;
+    for (let i = nums.length-1; i >= 0; i--) {
+        output[i] *= suffixProduct;
+        suffixProduct *= nums[i];
+    }
+    return output;
+    // Time Complexity: O(N)
+    // Space Complexity: O(1)
+};
+/*
+Mutate the input array.
+We will do two passes: one from the beginning to the end and one from the end to the beginning.
+After the first scan,
+nums[i] must be the product of all elements to its left.
+After the second scan,
+nums[i] must be the product of all elements to its left and all elements to its right.
+*/
+
