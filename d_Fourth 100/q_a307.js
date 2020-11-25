59284f4b0b350ef199317691e0f3e30feaaa9da5
hbjORbj <sldisek783@gmail.com>
2020-11-25 02:24:31 +0000

    Q307 Update

--- d_Fourth 100/q_a307.js
+++ d_Fourth 100/q_a307.js
@@ -6,3 +6,17 @@ Assume we are dealing with an environment that could only store integers within 
 
 For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 */
+
+var reverse = function(x) {
+    let arr = x.toString().split("");
+    let left = (arr[0] == '-') ? 1 : 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++, right--;
+    }
+    let res = Number(arr.join(""));
+    return (res >= -(2**31) && res <= 2**31-1) ? res : 0;
+    // Time Complexity: O(N) where N = length of input as string
+    // Space Complexity: O(N) where N = length of input as string
+};
