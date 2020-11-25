55fcf3c9a18217f6449d712cc64ee2a6a1b0463f
hbjORbj <sldisek783@gmail.com>
2020-11-25 02:12:43 +0000

    Q309 Update

--- d_Fourth 100/q_a309.js
+++ d_Fourth 100/q_a309.js
@@ -1,7 +1,34 @@
 /*
-Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 
-Return the answer in any order.
+Merge all the linked-lists into one sorted linked-list and return it.
+*/
 
-A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+var mergeKLists = function(lists) {
+    if (lists.length == 0) return null;
+    let newHead = new ListNode();
+    let cur = newHead;
+    while (true) {
+        let minIdx = null;
+        for (let i = 0; i < lists.length; i++) {
+            if (lists[i] && (minIdx == null || lists[i].val < lists[minIdx].val)) {
+                minIdx = i;
+            }
+        }   
+        if (minIdx == null) break; // there is no more node to traverse
+        cur.next = lists[minIdx];
+        cur = cur.next;
+        lists[minIdx] = lists[minIdx].next;
+    }
+    return newHead.next;
+    // Time Complexity: O(k * N), N = the number of all nodes | every selection of one node costs O(k)
+    // Space Complexity: O(1)
+};
+
+/*
+1. Create a new head node.
+2. Scan through the heads of every list and find the head with the smallest value.
+3. Append this node to our list. We advance pointer of our list by one.
+We advance the head with the smallest value by one as well.
+4. Repeat 1,2,3 until there is no more node to traverse.
 */
