7bade18deb6e137bf3ff14d960af66e62b9e40e9
hbjORbj <sldisek783@gmail.com>
2020-11-28 02:02:39 +0000

    Q332 Update

--- d_Fourth 100/q_a332.js
+++ d_Fourth 100/q_a332.js
@@ -3,3 +3,17 @@ Given an array where elements are sorted in ascending order, convert it to a hei
 
 For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 */
+
+var sortedArrayToBST = function(nums) {
+    return createBST(nums);
+    function createBST(nums) {
+        if (nums.length == 0) return null;
+        let mid = Math.floor(nums.length / 2);
+        let root = new TreeNode(nums[mid]);
+        root.left = createBST(nums.slice(0,mid));
+        root.right = createBST(nums.slice(mid+1));
+        return root;
+    }
+    // Time Complexity: O(N), we visit every element exactly once
+    // Space Complexity: O(N), call stack can go as deep as number of elements
+};
