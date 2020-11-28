49cb89fe346eb13a2e997109353901bd2b5074e8
hbjORbj <sldisek783@gmail.com>
2020-11-28 22:54:44 +0000

    Q339 Update

--- d_Fourth 100/q_a338.js
+++ d_Fourth 100/q_a338.js
@@ -3,3 +3,21 @@ You are climbing a staircase. It takes n steps to reach the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 */
+
+var climbStairs = function(n) {
+    // dp[i] represents # of ways to reach i-th floor
+    let dp = new Array(n+1); // size is n+1 because array is zero-indexed
+    dp[0] = 1, dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+};
+
+// # of ways to reach i-th floor = # of ways to reach i-1 floor + # of ways to reach i-2 floor
+// base cases: 
+// 1. # of ways to reach 0-th floor is 1
+// 2. # of ways to reach 1-st floor is 1
+
--- d_Fourth 100/q_a339.js
+++ d_Fourth 100/q_a339.js
@@ -11,3 +11,16 @@ Given a list of non-negative integers representing the amount of money of each h
 
 determine the maximum amount of money you can rob tonight without alerting the police.
 */
+
+var rob = function(nums) {
+    if (nums.length == 0) return 0;
+    // dp[i] represents the maximum amount of money robbed up to i-th house
+    let dp = new Array(nums.length+1);
+    dp[0] = 0, dp[1] = nums[0];
+    for (let i = 2; i <= nums.length; i++) {
+        dp[i] = Math.max(nums[i-1] + dp[i-2], dp[i-1]);
+    }
+    return dp[nums.length];
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+};
