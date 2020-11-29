973cd7162ca60a6f8760a5eed31326b96bc77ea3
hbjORbj <sldisek783@gmail.com>
2020-11-29 01:00:18 +0000

    Q348 Update

--- d_Fourth 100/q_a348.js
+++ d_Fourth 100/q_a348.js
@@ -2,3 +2,17 @@
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
 */
 
+var inorderTraversal = function(root) {
+    let values = [];
+    inorder(root);
+    return values;
+    
+    function inorder(root) {
+        if (!root) return;
+        inorder(root.left);
+        values.push(root.val);
+        inorder(root.right);
+    };
+    // Time Complexity: O(N), we visit every node exactly once
+    // Space Complexity: O(H), call stack can go as deep as height of tree
+};
