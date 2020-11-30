3a3e70412da010cb295859e9e3fa889cb627fb85
hbjORbj <sldisek783@gmail.com>
2020-11-30 22:07:33 +0000

    Q365 Update

--- d_Fourth 100/q_a365.js
+++ d_Fourth 100/q_a365.js
@@ -7,3 +7,22 @@ Note:
 - Both num1 and num2 does not contain any leading zero.
 - You must not use any built-in BigInteger library or convert the inputs to integer directly.
 */
+
+var addStrings = function(num1, num2) {
+    let idx1 = num1.length - 1, idx2 = num2.length - 1;
+    let carry = 0;
+    let res = "";
+    while (idx1 >= 0 || idx2 >= 0) {
+        let dig1 = idx1 >= 0 ? parseInt(num1[idx1--]) : 0;
+        let dig2 = idx2 >= 0 ? parseInt(num2[idx2--]) : 0;
+        let sum = dig1 + dig2 + carry;
+        res = (sum % 10) + res;
+        carry = Math.floor(sum / 10);
+    }
+    if (carry) {
+        res = carry + res;
+    }
+    return res;
+    // Time Complexity: O(max(m,n)), m = length of num1, n = length of num2
+    // Space Complexity: O(max(m,n))
+};
