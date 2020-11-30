00a68c13a507947724a939dd2a5b5eab752afe2f
hbjORbj <sldisek783@gmail.com>
2020-11-30 22:20:35 +0000

    Q368 Update

--- d_Fourth 100/q_a368.js
+++ d_Fourth 100/q_a368.js
@@ -1,3 +1,24 @@
 /*
 Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
 */
+
+var validPalindrome = function(s) {
+    return isPalindrome(0, s.length-1, 0);
+    
+    function isPalindrome(start, end, deleted) {
+        while (start < end) {
+            if (s[start] !== s[end]) {
+                if (deleted == 0) {
+                    return isPalindrome(start + 1, end, deleted + 1) || 
+                        isPalindrome(start, end - 1, deleted + 1);
+                }
+                else return false;
+            }
+            start++, end--;
+        }
+        return true;
+    }
+    // Time Complexity: O(N), we visit every letter once
+    // Space Complexity: O(1), call stack can go as deep as 1
+};
+
