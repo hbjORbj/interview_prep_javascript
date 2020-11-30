9c80219fca8008d05a34e763f09cb4d43e4c3179
hbjORbj <sldisek783@gmail.com>
2020-11-30 04:03:28 +0000

    Q346 Update

--- d_Fourth 100/q_a345.js
+++ d_Fourth 100/q_a345.js
@@ -5,3 +5,18 @@ For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73]
 
 Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100]. 
 */
+
+var dailyTemperatures = function(T) {
+    let stack = []; // stack stores temperatures looking for greater temperatures
+    let res = new Array(T.length).fill(0);
+    for (let i = 0; i < T.length; i++) {
+        while (stack.length > 0 && stack[stack.length - 1][0] < T[i]) {
+            let idx = stack.pop()[1];
+            res[idx] = i - idx;
+        }
+        stack.push([T[i], i]);
+    }
+    return res;
+    // Time Complexity: O(N)
+    // Space Complexity: O(N)
+};
