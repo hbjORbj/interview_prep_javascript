d6ae75be2a812d0769b6003abf1bb68da495ac60
hbjORbj <sldisek783@gmail.com>
2020-11-30 03:17:12 +0000

    Q355 Update

--- d_Fourth 100/q_a355.js
+++ d_Fourth 100/q_a355.js
@@ -3,3 +3,25 @@ Given a m x n grid filled with non-negative numbers, find a path from top left t
 
 Note: You can only move either down or right at any point in time.
 */
+
+// Without creating a dp table
+var minPathSum = function(grid) {
+    if (grid.length == 0 || grid[0].length == 0) return 0;
+    let height = grid.length, width = grid[0].length;
+    // we will mutate the given grid so that grid[i][j] is the minimum sum of path to (i, j)
+    // for the first row, the minimum path is coming from left so we simply add the previous left sum
+    // for the first column, the minimum path is coming from up so we simply add the previous upper sum
+    // else, we take the minimum of sum from up and sum from left and add current position's number to it
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (i == 0) grid[i][j] += j > 0 ? grid[i][j-1] : 0;
+            else if (j == 0) grid[i][j] += i > 0 ? grid[i-1][j] : 0;
+            else {
+                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+    }
+    return grid[height-1][width-1];
+    // Time Complexity: O(m * n)
+    // Space Complexity: O(1)
+};
---
+++ d_Fourth 100/q_a357.js
@@ -0,0 +1,3 @@
+/*
+Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+*/
